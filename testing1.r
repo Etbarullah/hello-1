c86be7d9aca3a2caeb503b37faf6ea28aba4d5b0
Etbarullah <ik3804803@gmail.com>
2022-03-04 01:04:58 +0500

    few lines added

--- testing1.r
+++ testing1.r
@@ -1,2 +1,9 @@
-This is my git assignment
-print('Hello Github')
+#This is my git assignment
+print("Hello Github")
+
+name = "iqbal khan"
+version = "RStudio"
+
+print(name)
+print("These lines have been written in RSudio")
+
